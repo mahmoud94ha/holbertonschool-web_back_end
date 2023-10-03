80c00f3cc5127683b3e04ddb782d21c0d6aa0244
unknown <mahmoudhannani3@gmail.com>
2023-10-03 11:36:38 +0100

    add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Sum two numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    a -> float
+    b -> float
+    return sum of a and b
+    """
+    return a+b

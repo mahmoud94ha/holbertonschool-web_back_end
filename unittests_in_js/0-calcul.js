f66ef2a8dc83242e48c002d922bdae244910d802
unknown <mahmoudhannani3@gmail.com>
2023-12-03 21:56:51 +0100

    task 0

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,20 +1,4 @@
-function calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-  
-    switch (type) {
-      case 'SUM':
-        return roundedA + roundedB;
-  
-      case 'SUBTRACT':
-        return roundedA - roundedB;
-  
-      case 'DIVIDE':
-        if (roundedB === 0) {
-          return 'Error';
-        }
-        return roundedA / roundedB;
-    }
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
   }
-  
   module.exports = calculateNumber;

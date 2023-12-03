e83377f355d97653eee4339c6b896632dde58077
unknown <mahmoudhannani3@gmail.com>
2023-12-03 21:56:51 +0100

    task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,4 +1,28 @@
-function calculateNumber(a, b) {
-    return Math.round(a) + Math.round(b);
-  }
-  module.exports = calculateNumber;
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber for SUM', () => {
+  it('check result for SUM', () => {
+    assert.equal(calculateNumber('SUM', 1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM', 2.8, 2), 5);
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber('SUM', 1.5, 1.2), 3);
+  });
+})
+
+describe('calculateNumber for SUBTRACT', () => {
+  it('should check the result of subtract', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 3, 1), 2);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 1.4), 4);
+  });
+});
+
+describe('calculateNumber for DIVIDE', () => {
+  it('should check the result of divide', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});

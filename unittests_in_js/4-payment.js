5685882887b26175f6b14b50d2b888690f84a037
unknown <mahmoudhannani3@gmail.com>
2024-01-15 22:36:52 +0100

    task 5

---
+++ unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
